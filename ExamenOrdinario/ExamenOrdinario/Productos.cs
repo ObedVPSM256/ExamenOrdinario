e89508fcf7ab9469a3202161ac7a59452d981013
ObedVPSM256 <105815117+ObedVPSM256@users.noreply.github.com>
2022-06-13 11:17:31 -0700

    EXAMEN ORDINARIO c:

---
+++ ExamenOrdinario/ExamenOrdinario/Productos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExamenOrdinario
+{
+    //declaración de clase pública
+    public class Productos
+    {
+        //atributos de la clase
+        public string Producto;
+        public int Cantidad;
+        public float Precio;
+
+        //constructor de clase
+        public Productos(string Producto, int Cantidad, float Precio)
+        {
+            this.Producto = Producto;
+            this.Cantidad = Cantidad;
+            this.Precio = Precio;
+        }
+
+        //metodos
+        public void MostrarArchivo()
+        {
+            Console.WriteLine("Producto: " + this.Producto);
+            Console.WriteLine("Cantidad: " + this.Cantidad);
+            Console.WriteLine("Precio: " + this.Precio);
+        }
+    }
+}
---
+++ ExamenOrdinario/ExamenOrdinario/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExamenOrdinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables auxiliares
+            string Producto;
+            int Cantidad;
+            float Precio;
+
+            StreamWriter sw = new StreamWriter("Beta.txt", true);
+
+            try
+            {
+                Console.WriteLine("*** ESCRIBA LOS DATOS SOLICITADOS ***");
+                Console.Write("Nombre del producto: ");
+                Producto = Console.ReadLine();
+                Console.Write("Cantidad del producto: ");
+                Cantidad = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Precio del producto: ");
+                Precio = float.Parse(Console.ReadLine());
+                //creación objeto
+                Productos Prod = new Productos(Producto, Cantidad, Precio);
+                sw.WriteLine(Prod.Producto + " " + Prod.Cantidad + " " + Prod.Precio);
+                //cerrar StreamWriter
+                sw.Close();
+                //limpiar consola
+                Console.Clear();
+                //método para mostrar archivo
+                Prod.MostrarArchivo();
+                //cerrar programa
+                Console.WriteLine("Presione <enter> para finalizar programa");
+                Console.ReadKey();
+            }
+
+            //captura de posibles errores
+            catch(IOException e)
+            {
+                Console.WriteLine("\nError : " + e.Message);
+                Console.WriteLine("\nRuta : " + e.StackTrace);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("\nError : " + e.Message);
+                Console.WriteLine("\nRuta : " + e.StackTrace);
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("\nError : " + e.Message);
+                Console.WriteLine("\nRuta : " + e.StackTrace);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("\nError : " + e.Message);
+                Console.WriteLine("\nRuta : " + e.StackTrace);
+            }
+        }
+    }
+}
